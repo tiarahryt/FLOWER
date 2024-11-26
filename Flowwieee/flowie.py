11275e0e15af3088bb05e3daf7891acd0a5d863e
thecodingdeveloper <tiaraharyanto4@gmail.com>
2024-11-26 11:03:57 +0700

    first commit

---
+++ Flowwieee/flowie.py
@@ -0,0 +1,28 @@
+import turtle
+
+li = turtle.Turtle()
+li.screen.bgcolor("black")
+li.pensize(2)
+li.color("green")
+li.left(100)
+li.backward(80)
+li.speed(6000)
+li.shape("turtle")
+
+def love (i):
+    if i<10:
+        return
+    else:
+        li.forward(i)
+        li.color("red")
+        li.circle(3)
+        li.color("green")
+        li.left(30)
+
+        love(3*i/4)
+        li.right(60)
+        love(3*i/4)
+        li.left(30)
+        li.backward(i)
+love(60)
+turtle.done
